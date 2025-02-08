c8d9d5e086970b46f8673b09d5925e2ae9d5419f
fortune4-ravishankarpal <ravishankar.pal@fortune4.in>
2025-02-08 16:13:07 +0530

    tested clame

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,43 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface CommonLink extends Struct.ComponentSchema {
+  collectionName: 'components_common_links';
+  info: {
+    description: '';
+    displayName: 'link';
+    icon: 'link';
+  };
+  attributes: {
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    linkImage: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    LinkName: Schema.Attribute.String;
+    linkPath: Schema.Attribute.String;
+  };
+}
+
+export interface CommonSectionName extends Struct.ComponentSchema {
+  collectionName: 'components_common_section_names';
+  info: {
+    description: '';
+    displayName: 'sectionName';
+    icon: 'bulletList';
+  };
+  attributes: {
+    link: Schema.Attribute.Component<'common.link', false>;
+    sectionDesc: Schema.Attribute.RichText &
+      Schema.Attribute.CustomField<
+        'plugin::ckeditor5.CKEditor',
+        {
+          preset: 'defaultHtml';
+        }
+      >;
+    SectionName: Schema.Attribute.String;
+  };
+}
+
 export interface SharedMedia extends Struct.ComponentSchema {
   collectionName: 'components_shared_media';
   info: {
@@ -46,6 +84,7 @@ export interface SharedSeo extends Struct.ComponentSchema {
   attributes: {
     metaDescription: Schema.Attribute.Text & Schema.Attribute.Required;
     metaTitle: Schema.Attribute.String & Schema.Attribute.Required;
+    rich: Schema.Attribute.Component<'shared.rich-text', false>;
     shareImage: Schema.Attribute.Media<'images'>;
   };
 }
@@ -62,14 +101,56 @@ export interface SharedSlider extends Struct.ComponentSchema {
   };
 }
 
+export interface SolutionSolution extends Struct.ComponentSchema {
+  collectionName: 'components_solution_solutions';
+  info: {
+    description: '';
+    displayName: 'industry';
+  };
+  attributes: {
+    ApplicationLIst: Schema.Attribute.Component<'common.link', true>;
+    desc: Schema.Attribute.Blocks;
+    detailDesc: Schema.Attribute.RichText &
+      Schema.Attribute.CustomField<
+        'plugin::ckeditor5.CKEditor',
+        {
+          preset: 'defaultHtml';
+        }
+      >;
+    images: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    lebal: Schema.Attribute.String;
+    note: Schema.Attribute.RichText;
+    tabIcon: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SolutionTest extends Struct.ComponentSchema {
+  collectionName: 'components_solution_tests';
+  info: {
+    displayName: 'test';
+  };
+  attributes: {};
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'common.link': CommonLink;
+      'common.section-name': CommonSectionName;
       'shared.media': SharedMedia;
       'shared.quote': SharedQuote;
       'shared.rich-text': SharedRichText;
       'shared.seo': SharedSeo;
       'shared.slider': SharedSlider;
+      'solution.solution': SolutionSolution;
+      'solution.test': SolutionTest;
     }
   }
 }
